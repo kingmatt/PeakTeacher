312ff38a45f04d154796aa1038901fc29edfcfea
Matt King <king.matthew@husky.neu.edu>
2014-11-22 22:15:38 -0500

    Initial commit of the rails app for the teacher app. TODO: Update README with instructions for running local machine, give other collaborators heroku credentials for the associated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeakTeacher::Application.config.secret_key_base = '7047e215993b4b0f718167ca9e49833d29dadc7f4e6469aa3054b555c025e13e05ca0d58cf72cbead21ea786a6fa57f6c26afde9441e18d2674d8c1fc3276848'
